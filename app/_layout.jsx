9d8eae3c928684b37cc04db974611544b339df63
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-06-10 00:02:03 +0530

    initial commit

---
+++ app/_layout.jsx
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Slot } from "expo-router";
+
+const RootLayout = () => {
+  return <Slot />;
+};
+
+export default RootLayout;
